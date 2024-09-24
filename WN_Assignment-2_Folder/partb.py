6eeb69bb76ec40f5fcbcd20c17fa5dffae3c9e41
arnav-s1ngh <92050142+arnav-s1ngh@users.noreply.github.com>
2024-09-24 22:45:46 +0530

    Update partb.py

--- WN_Assignment-2_Folder/partb.py
+++ WN_Assignment-2_Folder/partb.py
@@ -1,7 +1,13 @@
 import matplotlib.pyplot as plt
-y=[47,26,9,8,8]
-x=list(range(1,6))
-plt.plot(x,y)
-plt.xlabel("Nodes")
-plt.ylabel("Time in Seconds")
+import pandas as pd
+cap=pd.read_csv("partbcapture.csv")
+lst=list(cap["Time"])
+print(lst)
+t1=lst[len(lst)-1]-lst[0]
+t2=lst[len(lst)-2]-lst[1]
+t3=lst[len(lst)-3]-lst[2]
+t4=lst[len(lst)-4]-lst[3]
+t5=lst[len(lst)-5]-lst[4]
+print(t1,t2,t3,t4,t5)
+plt.plot([1,2,3,4,5],[t1,t2,t3,t4,t5])
 plt.show()

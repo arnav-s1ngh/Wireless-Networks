873fdc3c40a35414d6a32df3b2920b000c774c59
arnav-s1ngh <92050142+arnav-s1ngh@users.noreply.github.com>
2024-09-24 12:11:49 +0530

    Update WN_Assignment-2_Part-B.cc

--- WN_Assignment-2_Folder/WN_Assignment-2_Part-B.cc
+++ WN_Assignment-2_Folder/WN_Assignment-2_Part-B.cc
@@ -11,6 +11,7 @@
 #include "ns3/wifi-mac-helper.h"
 #include "ns3/yans-wifi-channel.h"
 #include "ns3/mobility-helper.h"
+#include <ctime>
 using namespace ns3;
 NS_LOG_COMPONENT_DEFINE("WN_Assignment-2");
 int main(int argc, char* argv[]) {
@@ -61,30 +62,24 @@ int main(int argc, char* argv[]) {
    address.SetBase("10.1.2.0", "255.255.255.0");
    Ipv4InterfaceContainer wifi_interfaces = address.Assign(wifi_devices);
    address.Assign(ap_device);
-
-  //Change the application
-  int cl_num=0;
-  BulkSendHelper source ("ns3::TcpSocketFactory", InetSocketAddress (wifi_interfaces.GetAddress (cl_num), 9));
-  source.SetAttribute ("MaxBytes", UintegerValue(5*1024*1024));
-  ApplicationContainer sourceApps = source.Install (p2p_nodes.Get (0));
-  sourceApps.Start (Seconds (0.0));
-  sourceApps.Stop (Seconds (50.0));
-
-  PacketSinkHelper sink ("ns3::TcpSocketFactory",InetSocketAddress (Ipv4Address::GetAny (), 9));
-  ApplicationContainer sinkApps = sink.Install (wifi_nodes.Get(cl_num));
-  sinkApps.Start (Seconds (0.0));
-  sinkApps.Stop (Seconds (50.0));
-  
-  
-  Ipv4GlobalRoutingHelper::PopulateRoutingTables(); 
-  Simulator::Stop(Seconds(50.0));
-  Time begin=Simulator::Now();
-  Simulator::Run();
-  Time end=Simulator::Now();
-  Simulator::Destroy();
-
-  Ptr<PacketSink> sink1 = DynamicCast<PacketSink> (sinkApps.Get (0));
-  std::cout << "Total Bytes Received: " << sink1->GetTotalRx () << std::endl;
-  std::cout<< "Time Taken" << end-begin<< std::endl;
-  return 0;
+   clock_t time_req;
+   clock_t tim2;
+   for(int cl_num=0;cl_num<nw;cl_num++){
+      time_req=clock();
+      BulkSendHelper source ("ns3::TcpSocketFactory", InetSocketAddress (wifi_interfaces.GetAddress (cl_num), 9));
+      source.SetAttribute ("MaxBytes", UintegerValue(5*1024*1024));
+      ApplicationContainer sourceApps = source.Install (p2p_nodes.Get (0));
+      sourceApps.Start (Seconds (0.0));
+      sourceApps.Stop (Seconds (50.0));
+      time_req=clock()-time_req;
+      std::cout<<"[DOWNLOAD] Time Taken for Client " << cl_num+1<< " is " << time_req<< std::endl;
+   }
+   Ipv4GlobalRoutingHelper::PopulateRoutingTables(); 
+   Simulator::Stop(Seconds(50.0));
+   Time begin=Simulator::Now();
+   Simulator::Run();
+   Time end=Simulator::Now();
+   Simulator::Destroy();
+   std::cout<< "Simulation Time" << end-begin<< std::endl;
+   return 0;
 }

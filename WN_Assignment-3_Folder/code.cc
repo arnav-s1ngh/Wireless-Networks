7fcaebc6a3c5cd9b97541f946271b20c0497a5b1
arnav-s1ngh <92050142+arnav-s1ngh@users.noreply.github.com>
2024-11-05 14:02:43 +0530

    Update code.cc

--- WN_Assignment-3_Folder/code.cc
+++ WN_Assignment-3_Folder/code.cc
@@ -62,7 +62,7 @@ int main(){
     NetDeviceContainer sta_devices=wifi.Install(phy,mac,sta_nodes);
     
     MobilityHelper mobility;
-    mobility.SetPositionAllocator("ns3::GridPositionAllocator","MinX",DoubleValue(0.0),"MinY", DoubleValue(0.0),"DeltaX",DoubleValue(5.0),"DeltaY", DoubleValue(0.0),"GridWidth",UintegerValue(2),"LayoutType", StringValue("RowFirst"));
+    mobility.SetPositionAllocator("ns3::GridPositionAllocator","MinX",DoubleValue(0.0),"MinY", DoubleValue(0.0),"DeltaX",DoubleValue(1),"DeltaY", DoubleValue(0.0),"GridWidth",UintegerValue(2),"LayoutType", StringValue("RowFirst"));
     
     mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
     mobility.Install(ap_node);
@@ -88,8 +88,46 @@ int main(){
     ap_interface=address.Assign(ap_device);
     sta_interface=address.Assign(sta_devices);
     
-}
+    int bs_port=9;
+    int oo_port=40;
+    
+    for (int i=0;i<20;i++){
+        BulkSendHelper source("ns3::TcpSocketFactory",InetSocketAddress(sta_interface.GetAddress(i),bs_port));
+        source.SetAttribute("MaxBytes",UintegerValue(5*1024*1024));
+        ApplicationContainer sourceApps=source.Install(server_node.Get(0));
+        sourceApps.Start(Seconds(0.0));
+        sourceApps.Stop(Seconds(10.0));
+        
+        PacketSinkHelper sink("ns3::TcpSocketFactory",InetSocketAddress(Ipv4Address::GetAny(),bs_port));
+        ApplicationContainer sinkApps=sink.Install(sta_nodes.Get(i));
+        sinkApps.Start(Seconds(0.0));
+        sinkApps.Stop(Seconds(10.0));
+        bs_port+=1;
+        
+        OnOffHelper source2("ns3::TcpSocketFactory",InetSocketAddress(p2p_interface.GetAddress(1),oo_port));
+        source2.SetAttribute("DataRate",StringValue("200Kbps"));
+        ApplicationContainer sourceApps2=source2.Install(sta_nodes.Get(i));
+        sourceApps2.Start(Seconds(1.0));
+        sourceApps2.Stop(Seconds(10.0));
+        
+        PacketSinkHelper sink2("ns3::TcpSocketFactory",InetSocketAddress(Ipv4Address::GetAny(),oo_port));
+        ApplicationContainer sinkApps2=sink2.Install(server_node.Get(0));
+        sinkApps2.Start(Seconds(0.0));
+        sinkApps2.Stop(Seconds(10.0));
+        oo_port+=1;
+    }
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables(); 
+    p2pconn.EnablePcapAll("wired_capture",false);
+    phy.EnablePcapAll("wireless_capture", false);
+    
+    Simulator::Stop(Seconds(10.0));
+    
+    Simulator::Run();
+    Simulator::Destroy();
 
+    
+    
+}
 
 
 

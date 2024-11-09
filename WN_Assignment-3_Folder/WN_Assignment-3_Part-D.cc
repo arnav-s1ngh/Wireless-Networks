13180b9e954bb79cdcd7cf206bf829d07ef79129
arnav-s1ngh <92050142+arnav-s1ngh@users.noreply.github.com>
2024-11-09 19:18:57 +0530

    Update WN_Assignment-3_Part-D.cc

--- WN_Assignment-3_Folder/WN_Assignment-3_Part-D.cc
+++ WN_Assignment-3_Folder/WN_Assignment-3_Part-D.cc
@@ -1,1 +1,232 @@
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/network-module.h"
+#include "ns3/packet-sink.h"
+#include "ns3/wifi-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/netanim-module.h"
+#include "ns3/point-to-point-module.h"
+#include <fstream>
+#include <string>
 
+
+#include "ns3/attribute-container.h"
+#include "ns3/boolean.h"
+#include "ns3/command-line.h"
+#include "ns3/config.h"
+#include "ns3/double.h"
+#include "ns3/enum.h"
+#include "ns3/he-phy.h"
+#include "ns3/internet-stack-helper.h"
+#include "ns3/ipv4-address-helper.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/log.h"
+#include "ns3/mobility-helper.h"
+#include "ns3/multi-model-spectrum-channel.h"
+#include "ns3/on-off-helper.h"
+#include "ns3/packet-sink-helper.h"
+#include "ns3/packet-sink.h"
+#include "ns3/spectrum-wifi-helper.h"
+#include "ns3/ssid.h"
+#include "ns3/string.h"
+#include "ns3/udp-client-server-helper.h"
+#include "ns3/udp-server.h"
+#include "ns3/uinteger.h"
+#include "ns3/wifi-acknowledgment.h"
+#include "ns3/yans-wifi-channel.h"
+#include "ns3/yans-wifi-helper.h"
+ 
+#include <algorithm>
+#include <functional>
+
+using namespace ns3;
+NS_LOG_COMPONENT_DEFINE("WN_Assign-3");
+
+// Response Time
+int packet_cnt=0;
+double resp_time=0;
+
+// Collision and reception counters
+int totalReceptions=0;
+int totalCollisions=0;
+
+// Track request and response times
+std::map<uint32_t,Time> packetSendTimes; // Map packet ID to send time
+
+void PacketReception(std::string context,Ptr<const Packet> packet,double snr,WifiMode mode,WifiPreamble preamble) {
+    totalReceptions++;
+}
+
+void PacketCollision(std::string context,Ptr<const Packet> packet,double snr) {
+    totalCollisions++;
+}
+
+// Callback for PHY transmission
+void PhyTxTrace(std::string context,Ptr<const Packet> packet,WifiMode mode,WifiPreamble preamble,uint8_t txPower) {
+    // Store the transmission time for each packet based on its ID
+    packetSendTimes[packet->GetUid()]=Simulator::Now();
+}
+
+// Callback for PHY reception
+void PhyRxOkTrace(std::string context,Ptr<const Packet> packet,double snr,WifiMode mode,WifiPreamble preamble) {
+    uint32_t packetId=packet->GetUid();
+    
+    if (packetSendTimes.find(packetId) != packetSendTimes.end()) {
+        // Calculate the response time
+        Time sendTime=packetSendTimes[packetId];
+        Time receiveTime=Simulator::Now();
+        Time responseTime=receiveTime-sendTime;
+        
+        //std::cout << "Packet ID: " << packetId << ",Response time: " << responseTime.GetMicroSeconds() << " microseconds" << std::endl;
+        resp_time+=responseTime.GetMicroSeconds();
+        packet_cnt+=1;
+        
+        // Remove the entry to keep the map clean
+        packetSendTimes.erase(packetId);
+    }
+}
+
+void simulation(int ru_count, bool cent) {
+    //Config::SetDefault("ns3::WifiDefaultAckManager::DlMuAckSequenceType",EnumValue(WifiAcknowledgment::DL_MU_BAR_BA_SEQUENCE));
+    //Config::SetDefault("ns3::WifiDefaultAckManager::DlMuAckSequenceType",EnumValue(WifiAcknowledgment::DL_MU_TF_MU_BAR));
+    //Config::SetDefault("ns3::WifiDefaultAckManager::DlMuAckSequenceType",EnumValue(WifiAcknowledgment::DL_MU_AGGREGATE_TF));
+    int nw=20; // Number of STAs in the WiFi network
+    NodeContainer sta_nodes;
+    sta_nodes.Create(nw);
+    
+    NodeContainer ap_node; // AP
+    ap_node.Create(1);
+    
+    NodeContainer server_node; // Server
+    server_node.Create(1);
+
+    // Establish Server-AP P2P Connection
+    PointToPointHelper p2pconn;
+    p2pconn.SetDeviceAttribute("DataRate",StringValue("1000Mbps"));
+    p2pconn.SetChannelAttribute("Delay",StringValue("100ms"));
+    NodeContainer p2p_nodes;
+    NetDeviceContainer p2p_device;
+    p2p_nodes.Add(ap_node);
+    p2p_nodes.Add(server_node);
+    p2p_device=p2pconn.Install(p2p_nodes);
+    
+    // Wifi setup
+    auto spectrumChannel = CreateObject<MultiModelSpectrumChannel>();
+    Ptr<LogDistancePropagationLossModel> lossModel = CreateObject<LogDistancePropagationLossModel>(); 
+    spectrumChannel->AddPropagationLossModel(lossModel);
+    Ptr<NakagamiPropagationLossModel> fadingModel = CreateObject<NakagamiPropagationLossModel> ();
+    spectrumChannel->AddPropagationLossModel (fadingModel);
+
+    WifiHelper wifi;
+    wifi.SetStandard(WIFI_STANDARD_80211ax);
+    wifi.SetRemoteStationManager("ns3::MinstrelHtWifiManager");
+
+    StringValue value("{38, 40, BAND_5GHZ, 0}");
+
+    SpectrumWifiPhyHelper phy;
+    phy.SetPcapDataLinkType(WifiPhyHelper::DLT_IEEE802_11_RADIO);
+    phy.SetChannel(spectrumChannel);
+    
+    WifiMacHelper mac;
+    Ssid ssid=Ssid("ns-3-ssid");
+
+    // STAs
+    phy.Set("ChannelSettings", value);
+    mac.SetType("ns3::StaWifiMac","Ssid",SsidValue(ssid));
+    NetDeviceContainer sta_devices=wifi.Install(phy,mac,sta_nodes);
+
+    // AP
+    mac.SetMultiUserScheduler("ns3::RrMultiUserScheduler","EnableUlOfdma",BooleanValue(true),"EnableBsrp",BooleanValue(true),"NStations", UintegerValue (ru_count),"UseCentral26TonesRus", BooleanValue (cent));
+    mac.SetType("ns3::ApWifiMac","Ssid",SsidValue(ssid));
+    NetDeviceContainer ap_device=wifi.Install(phy,mac,ap_node.Get(0));
+
+    
+    MobilityHelper mobility;
+    mobility.SetPositionAllocator("ns3::GridPositionAllocator","MinX",DoubleValue(0.0),"MinY",DoubleValue(0.0),"DeltaX",DoubleValue(1),"DeltaY",DoubleValue(0.0),"GridWidth",UintegerValue(2),"LayoutType",StringValue("RowFirst"));
+    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    mobility.Install(ap_node);
+    mobility.Install(sta_nodes);
+    mobility.Install(server_node);
+    
+    // Install internet stack
+    InternetStackHelper internet;
+    internet.Install(ap_node);
+    internet.Install(sta_nodes);
+    internet.Install(server_node);
+    
+    Ipv4AddressHelper address;
+    address.SetBase("10.1.1.0","255.255.255.0");
+    Ipv4InterfaceContainer p2p_interface;
+    p2p_interface=address.Assign(p2p_device);
+ 
+    address.SetBase("10.1.2.0","255.255.255.0");
+    Ipv4InterfaceContainer ap_interface;
+    Ipv4InterfaceContainer sta_interface;
+    ap_interface=address.Assign(ap_device);
+    sta_interface=address.Assign(sta_devices);
+    
+    int bs_port=9;
+    int oo_port=40;
+    
+    for (int i=0; i < 20; i++) {
+        BulkSendHelper source("ns3::TcpSocketFactory",InetSocketAddress(sta_interface.GetAddress(i),bs_port));
+        source.SetAttribute("MaxBytes",UintegerValue(5 * 1024 * 1024));
+        ApplicationContainer sourceApps=source.Install(server_node.Get(0));
+        sourceApps.Start(Seconds(0.0));
+        sourceApps.Stop(Seconds(10.0));
+        
+        PacketSinkHelper sink("ns3::TcpSocketFactory",InetSocketAddress(Ipv4Address::GetAny(),bs_port));
+        ApplicationContainer sinkApps=sink.Install(sta_nodes.Get(i));
+        sinkApps.Start(Seconds(0.0));
+        sinkApps.Stop(Seconds(10.0));
+        bs_port+=1;
+        
+        OnOffHelper source2("ns3::TcpSocketFactory",InetSocketAddress(p2p_interface.GetAddress(1),oo_port));
+        source2.SetAttribute("DataRate",StringValue("200Kbps"));
+        ApplicationContainer sourceApps2=source2.Install(sta_nodes.Get(i));
+        sourceApps2.Start(Seconds(1.0));
+        sourceApps2.Stop(Seconds(10.0));
+        
+        PacketSinkHelper sink2("ns3::TcpSocketFactory",InetSocketAddress(Ipv4Address::GetAny(),oo_port));
+        ApplicationContainer sinkApps2=sink2.Install(server_node.Get(0));
+        sinkApps2.Start(Seconds(0.0));
+        sinkApps2.Stop(Seconds(10.0));
+        oo_port+=1;
+    }
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables(); 
+    p2pconn.EnablePcapAll("wired_capture",false);
+    phy.EnablePcapAll("wireless_capture",false);
+
+    // Connect to reception and collision events
+    Config::Connect("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Phy/State/RxOk",MakeCallback(&PacketReception));
+    Config::Connect("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Phy/State/RxError",MakeCallback(&PacketCollision));
+    
+    // Connect to PHY Tx and RxOk events for response time calculation
+    Config::Connect("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Phy/State/Tx",MakeCallback(&PhyTxTrace));
+    Config::Connect("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Phy/State/RxOk",MakeCallback(&PhyRxOkTrace));
+    
+    Simulator::Stop(Seconds(10.0));
+    Simulator::Run();
+    
+    // Calculate collision percentage
+    double collisionPercentage=(totalCollisions / (double)(totalReceptions+totalCollisions))*100;
+    std::cout << "Collision Percentage: " << collisionPercentage << "%" << std::endl;
+    std::cout<<"Average Response Time: "<<resp_time/packet_cnt<<" microseconds"<<std::endl;
+
+    Simulator::Destroy();
+}
+
+int main(){
+   std::cout<<"2x242 Tones"<<std::endl;
+   simulation(2,false);
+   std::cout<<"4x106 Tones"<<std::endl;
+   simulation(4,false);
+   std::cout<<"4x106 + Central 26 Tones"<<std::endl;
+   simulation(4,true);
+   std::cout<<"8x52 Tones"<<std::endl;
+   simulation(8,false);
+   std::cout<<"18x26 Tones"<<std::endl;
+   simulation(18,false);
+   return 0;
+}

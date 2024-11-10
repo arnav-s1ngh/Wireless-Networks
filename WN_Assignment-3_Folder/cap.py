0d07b8b004d109093a94d2241ddabf45d7ad9114
arnav-s1ngh <92050142+arnav-s1ngh@users.noreply.github.com>
2024-11-11 03:07:25 +0530

    Update cap.py

--- WN_Assignment-3_Folder/cap.py
+++ WN_Assignment-3_Folder/cap.py
@@ -3,8 +3,8 @@ cap=pd.read_csv("tcpsend.csv")
 for i in range(2, 22):
     dest="10.1.2." + str(i)
     tim=[]
-    for noth, row in cap.iterrows():
-        if row['Destination'] == dest:
+    for noth,row in cap.iterrows():
+        if row['Destination']==dest:
             tim.append(row['Time'])
     if tim!=[]:
         print(dest,max(tim)-min(tim))
